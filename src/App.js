575f556dc7e0f67bdffd31d4fc2dd9a760d3f6d2
NinelBenyush <ninelbenush@gmail.com>
2024-07-29 11:31:16 +0300

    Update React Router to v6

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Todo from "./components/Todo";
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes   } from 'react-router-dom';
 
 function App() {
   return (
@@ -11,10 +11,10 @@ function App() {
     <div className="d-flex flex-column min-vh-100">
       <Navbar />
       <div className="flex-grow-1">
-        <Routes>
+        <Routes >
         <Route path="/" element={<Hero />} />
-        <Route path="todo-list" element={<Todo />} />
-        </Routes>
+        <Route path="/todo-list" element={<Todo />} />
+        </Routes >
       </div>
       <Footer />
     </div>
